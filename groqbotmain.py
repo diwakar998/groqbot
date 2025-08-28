fa32d6fe8383a43c6dfd2b8a925d8f4f5c5a36e1
diwakar998 <34593700+diwakar998@users.noreply.github.com>
2025-08-28 14:09:06 +0530

    Update groqbotmain.py

--- groqbotmain.py
+++ groqbotmain.py
@@ -9,15 +9,18 @@ client = Groq(
 st.title("🤖🤖 PMO Groq Reporting & Governance Agent, Your PMO Expert")
 # User input
 user_input = st.chat_input("How can I help you today...")
+#prompt=chatprompttemplate.
 
 chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Explain the importance of fast language models",
+            #"content": "Explain the importance of fast language models",
+            "content": user_input,
         }
     ],
     model="llama-3.3-70b-versatile",
 )
-
-print(chat_completion.choices[0].message.content)
+outvar=chat_completion.choices[0].message.content
+st.write(outvar)
+print(outvar)
