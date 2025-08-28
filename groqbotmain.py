b0109ac225499b218e54b0f82816be92cdd7675d
diwakar998 <34593700+diwakar998@users.noreply.github.com>
2025-08-28 16:31:39 +0530

    Update groqbotmain.py

--- groqbotmain.py
+++ groqbotmain.py
@@ -19,7 +19,11 @@ LANGSMITH_PROJECT="test"
 #LANGCHAIN_API_KEY=st.secrets["LANGCHAIN_API_KEY"]
 from langchain_groq import ChatGroq
 
-llm = ChatGroq()
+#llm = ChatGroq()
+llm = ChatGroq(
+    groq_api_key=st.secrets["GROQ_API_KEY"],
+    model="llama-3.3-70b-versatile"   # or "llama3-70b-8192", etc.
+)
 llm.invoke("Hello, world!")
 '''
 prompt=ChatPromptTemplate.from_messages(
