c6f8f4955a06137863e2024f10ab51129f84f7c4
diwakar998 <34593700+diwakar998@users.noreply.github.com>
2025-08-28 16:13:01 +0530

    Update groqbotmain.py

--- groqbotmain.py
+++ groqbotmain.py
@@ -5,7 +5,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import groq
+#from langchain_community.llms import groq
 #import streamlit as st
 #import os
 #from dotenv import load_dotenv
