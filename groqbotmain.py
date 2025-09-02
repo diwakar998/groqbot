e674afdacdc4f5e85003267fff7834c61eab4536
diwakar998 <34593700+diwakar998@users.noreply.github.com>
2025-09-02 14:11:53 +0530

    Update groqbotmain.py

--- groqbotmain.py
+++ groqbotmain.py
@@ -161,7 +161,10 @@ if uploaded_file is not None:
                     row_text=""
                     contentTbl += row_text + "\n"
         text_data=content + contentTbl
-        st.write("the text file data is :)
+        st.write("the text file data is :")
+        st.write(content)
+        st.write("the text file data is :")
+        st.write(contentTbl)
         st.write(text_data)
 
     elif file_type == "csv":
